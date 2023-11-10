a744527995b182db2278c51a26fbdadfe73628bf
yonding <uwrgoy7584@khu.ac.kr>
2023-11-11 05:27:24 +0900

    배경색 변경

--- pages/face/index.tsx
+++ pages/face/index.tsx
@@ -6,7 +6,7 @@ export default function FacePage() {
   return (
     <div
       className="md-20 flex flex-col items-center justify-center px-6 pt-10 pb-20 sm:px-10 md:px-14"
-      style={{ backgroundColor: "black" }}
+      style={{ backgroundColor: "#00001E" }}
     >
       <img className="" src="/title.png" style={{}} alt="" />
       <FaceWidgets />
